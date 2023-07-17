359e8c60e9c00cf7c698a19bdaf684051fec8a59
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-07-17 18:14:40 +0300

    Update MaterialItem.jsx

--- src/components/MaterialItem.jsx
+++ src/components/MaterialItem.jsx
@@ -1,20 +1,46 @@
-export const MaterialItem = ({item, onDelete, onUpdate}) => {
-    return (
-       <div>
-       <p>Title: {item.title}</p>
-       <p>Link: {item.link}</p>
-       <button 
-           type="button" 
-           onClick={() => onDelete(item.id)}>
-           Delete
-       </button>
-       <button 
-           type="button" 
-           onClick={() => onUpdate({ id: item.id, title: Date.now() })}
-           >
-               Update
-       </button>
-       </div>
-    );
+import { Component } from "react"
+
+const EditMaterialModal = ({onClose, onEdit}) => {
+    return <div>
+        <h2>Модалка редактирования материала</h2>
+        <button type="button" onClick={() => {onEdit(); onClose()}}>Вот теперь точно редактировать</button>
+        <button type="button" onClick={onClose}>Close</button>
+    </div>
+}
+
+export class MaterialItem extends Component {
+
+    state = {
+        isModalOpen: false,
+    };
+
+    openModal = () => this.setState({isModalOpen: true});
+    closeModal = () => this.setState({isModalOpen: false});
+
+    render() {
+
+        const { item, onUpdate, onDelete } = this.props;
+        const { isModalOpen } = this.state;
+
+        return (
+            <div>
+            <p>Title: {item.title}</p>
+            <p>Link: {item.link}</p>
+            <button 
+                type="button" 
+                onClick={() => onDelete(item.id)}>
+                Delete
+            </button>
+            <button 
+                type="button" 
+                onClick={this.openModal}
+             //    onClick={() => onUpdate({ id: item.id, title: Date.now() })}
+                >
+                    Update
+            </button>
+            {isModalOpen && <EditMaterialModal onClose={this.closeModal} onEdit={() => onUpdate({ id: item.id, title: Date.now() })}/>}
+            </div>
+         );
+    }
 
 };
